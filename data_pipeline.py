f56c0a4a822f39a88935cc03fea5ff20b1bb4e1f
Lamel <lamel466@gmail.com>
2025-06-21 12:29:02 +0200

    workflow

--- data_pipeline.py
+++ data_pipeline.py
@@ -15,6 +15,7 @@ def main():
 
     # 3. Load (upload to S3)
     bucket_name = "databucket-v1"
+    upload_df_to_s3(df_raw, bucket_name, "raw_data/raw_sales.csv")
     upload_df_to_s3(customers, bucket_name, "transformed_data/customers.csv")
     upload_df_to_s3(stores, bucket_name, "transformed_data/stores.csv")
     upload_df_to_s3(products, bucket_name, "transformed_data/products.csv")
--- extract.py
+++ extract.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import logging
 import time
@@ -8,20 +9,23 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-  # ==========================================================
-# Extracting the data
-# ==========================================================
 def extract_data() -> pd.DataFrame:
     """Extracts data from Mockaroo API and returns a DataFrame."""
     logger.info("Starting data extraction from Mockaroo API...")
-    api = "https://api.mockaroo.com/api/0935e020?count=200&key=d34db810"
+
+    api_key = os.getenv("MOCKAROO_API_KEY")
+    if not api_key:
+        logger.error("MOCKAROO_API_KEY environment variable not set")
+        raise ValueError("Missing MOCKAROO_API_KEY")
+
+    api_url = f"https://api.mockaroo.com/api/0935e020?count=200&key={api_key}"
+
     start_time = time.perf_counter()
     try:
-        df = pd.read_csv(api)
+        df = pd.read_csv(api_url)
         elapsed = time.perf_counter() - start_time
         logger.info(f"Extracted {len(df)} records in {elapsed:.2f} seconds")
         return df
     except Exception as e:
         logger.error(f"Extraction failed: {e}")
         raise
-
